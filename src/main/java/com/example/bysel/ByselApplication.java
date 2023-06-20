6b280a5721a22f890b9c3ebc015a58881dec1f59
lelek337 <ioan.af@mail.ru>
2023-06-20 14:05:43 +0300

    my first config in this project

---
+++ src/main/java/com/example/bysel/ByselApplication.java
@@ -0,0 +1,13 @@
+package com.example.bysel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByselApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bysel/ByselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bysel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
